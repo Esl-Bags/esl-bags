ea04b481539b744634196a1b738d7c767d162f79
FilipeNeiva <filipevilanovaneiva@gmail.com>
2024-03-12 11:43:20 -0300

    componente header (incompleto)

---
+++ src/components/CustomHeader.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="flex justify-around w-full p-3 bg-brand-main">
+        <p>teste</p>
+        <p>teste</p>
+    </div>
+</template>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,3 +1,40 @@
 <template>
-    <h1 class="text-3xl font-bold underline">Pagina principal</h1>
+    <custom-header
+        @create-account="goToAccountCreate"
+        @login="goToLogin"
+    />
+    <carousel />
+    <offer />
+    <category />
+    <div class="flex justify-center py-10 bg-brand-gray">
+        <p class="font-medium text-center text-gray-800">Esl Bags &copy; 2024</p>
+    </div>
 </template>
+
+<script>
+import {onMounted} from 'vue'
+import CustomHeader from '../../components/CustomHeader.vue'
+import { useRouter } from 'vue-router'
+export default {
+    components: { CustomHeader },
+    setup () {
+        const router = useRouter()
+
+        onMounted(() => {
+            const token = window.localStorage.getItem('token')
+        })
+        function goToLogin() {
+            router.push({ name: 'Login' })
+        }
+
+        function goToAccountCreate() {
+            router.push({ name: 'Singup' })
+        }
+
+        return {
+            goToLogin,
+            goToAccountCreate
+        }
+    }
+}
+</script>
