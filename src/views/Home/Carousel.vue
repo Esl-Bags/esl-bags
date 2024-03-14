efecf5dceced937e3f0484f5054ba3bec5e190cf
FilipeNeiva <filipevilanovaneiva@gmail.com>
2024-03-14 14:14:35 -0300

    test fix bug image

--- src/views/Home/Carousel.vue
+++ src/views/Home/Carousel.vue
@@ -1,7 +1,7 @@
 <template>
   <carousel :items-to-show="1" :wrapAround="true" :autoplay="10000">
     <slide v-for="(image, index) in images" :key="index">
-      <img :src="'/src/assets/images/' + image.src" alt="...">
+      <img :src="image.src" alt="...">
     </slide>
 
     <template #addons>
@@ -13,6 +13,9 @@
 <script>
 import 'vue3-carousel/dist/carousel.css'
 import { Carousel, Slide, Navigation } from 'vue3-carousel'
+import image1 from '/src/assets/images/1.jpeg'
+import image2 from '/src/assets/images/2.jpeg'
+import image3 from '/src/assets/images/3.jpeg'
 
 export default {
   name: 'App',
@@ -24,12 +27,12 @@ export default {
 
   setup() {
     const images = [
-      { 'src': '1.jpeg' },
-      { 'src': '2.jpeg' },
-      { 'src': '3.jpeg' },
+      { 'src': image1 },
+      { 'src': image2 },
+      { 'src': image3 },
     ]
 
-    return { images }
+    return { images, image1 }
   }
 }
 </script>
